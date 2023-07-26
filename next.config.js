9181de43eb29f054509a7c2e9255cbb7ba41f343
twinkert <thomas.winkert@gmail.com>
2023-07-26 00:15:47 -0400

    updates 2

--- next.config.js
+++ next.config.js
@@ -23,18 +23,6 @@ console.warn(
   // "Are you publishing to <username>.github.io ? then [basePath] should be empty.\n" +
   // "Are you publishing to <username>.github.io/<repository> ? then [basePath] should be /<repository>.\n" +
   //module.exports = nextConfig
-  /*
-   {
-
-  webpack: (config, { buildId, dev, isServer, defaultLoaders, webpack }) => {
-    config.module.rules.push({
-      test: [/\.bmp$/, /\.gif$/, /\.jpe?g$/, /\.png$/],
-      loader: require.resolve("url-loader")
-    });
-
-    return config;
-  }
-};*/
   `P.S. [basePath] is {${basePath}}`
 )
 
@@ -47,9 +35,7 @@ const nextConfig = {
   },
 }
 
-module.exports = nextConfig,
-
-
+module.exports = nextConfig, {
 
   webpack: (config, { buildId, dev, isServer, defaultLoaders, webpack }) => {
     config.module.rules.push({
@@ -58,6 +44,7 @@ module.exports = nextConfig,
     });
 
     return config;
-  };
+  }
+};
 
 
